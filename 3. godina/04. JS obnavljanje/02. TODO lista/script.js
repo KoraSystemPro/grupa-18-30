a3697e14ddd40aefffaac7a041b83cb3dd8f33c8
Luka <lukakorica@gmail.com>
2020-12-06 19:55:59 +0100

    Dodata nedelja 6.12.2020. bez precrtavanja

--- 3. godina/04. JS obnavljanje/02. TODO lista/script.js
+++ 3. godina/04. JS obnavljanje/02. TODO lista/script.js
@@ -1,10 +1,40 @@
+var ul = document.getElementById("todoLista");
+var close = document.getElementsByClassName("close");
+
+ul.addEventListener("click", function(evt){
+    if(evt.target.tagName === "li"){
+        evt.target.classList.toggle = "gotovo";
+    }
+}, false);
+
+function sakrijiElement(){
+    let element = this.parentElement;
+    element.style.display = "none";
+}
+
 function napraviNoviElement(){
     let li = document.createElement("li");
-    let ulaznaVrednost = document.getElementById("input").value;
-    let t = document.createTextNode(ulaznaVrednost);
-    li.appendChild(t);
-    document.getElementById("myList").appendChild(li);
+    let ulaznaVrednost = document.getElementById("inputSadrzaj").value;
+    let ulazCvor = document.createTextNode(ulaznaVrednost);
+    li.appendChild(ulazCvor);
+    if(ulaznaVrednost == ""){
+        alert("Morate da napišete nešto!");
+    } else {
+        ul.appendChild(li);
+    }
+    // Brisemo sadrzaj input-a kada ga dodamo na listu
+    document.getElementById("inputSadrzaj").value = "";
+
+    let span = document.createElement("span");
+    span.className = "close";
+    span.appendChild(document.createTextNode("\u274C"));
+    li.appendChild(span);
+
+    // Za svaki clan liste pravimo X koje ce ga ugasiti
+    for(i = 0; i < close.length; i++){
+        close[i].addEventListener("click", sakrijiElement);
+    }
     
 }
 
-document.getElementById("input").addEventListener("click", napraviNoviElement);
+document.getElementById("dgmDodaj").addEventListener("click", napraviNoviElement);
