f6d4b0dd6606bb4a98917569c799f518e4f9b8e6
Luka Korica <lukakorica@gmail.com>
2021-02-09 19:22:26 +0100

    Postavljen vikend 06-07. FEB 2021

---
+++ 3. godina/05. Objektno orjentisano programiranje/03. C#/03. Bankovni racun/BankovniRacun/BankovniRacun/EmptyClass.cs
@@ -0,0 +1,145 @@
+﻿using System;
+namespace BankovniRacun
+{
+    //public class Vozilo
+    //{
+    //    string marka;
+    //    string model;
+    //    string boja;
+    //    double prenosni_kapaitet;
+    //    double potrosnja;
+    //    double rezervoar;
+    //    int br_ljudi;
+
+    //}
+    //public class ServisnaKnjizica
+    //{
+    //    string ime_izvodjaca;
+    //    string datum_rada;
+    //    double cena_rada;
+    //}
+    //public class TeretnoVozilo : Vozilo
+    //{
+    //    int br_tockova;
+    //    string tip_tereta;
+
+    //}
+
+    //public class Avion : Vozilo
+    //{
+    //    double cena_odrzavanja;
+    //    string vlasnik;
+    //    ServisnaKnjizica servisna_knjzica;
+    //}
+
+
+    public class Osoba
+    {
+        /*
+            Osoba ->
+               string ime
+               string prezime
+               string adresa
+               string telefon
+               int visina
+               double tezine
+               int uk_osoba -> Koliko ima osoba ukupno      
+
+               // Konstruktor
+               ime, prezime, adresa, telefon
+               ime, prezime, adresa, telefon, visina, tezina           
+
+        */
+        /*
+            Radnik ->
+               string ime
+               string prezime
+               string adresa
+               string telefon
+               int visina
+               double tezine
+               double satnica;
+               int br_radnih_sati;
+               string sektor;     // Drzavno/privatno
+               string zanimanje;
+
+               // Konstruktor
+               ime, prezime, adresa, telefon
+               ime, prezime, adresa, telefon, visina, tezina  
+               ime, prezime, adresa, telefon, visina, tezina, satnica, br_radnih_sati, sektor, zanimanje    
+         
+        */
+        public string ime;
+        public string prezime;
+        public string adresa;
+        public string telefon;
+        public int visina;
+        public double tezina;
+        public int uk_osoba = 0;
+
+        public Osoba()
+        {
+            string ime = null;
+            string prezime = null;
+            string adresa = null;
+            string telefon = null;
+            int visina = 0;
+            double tezina = 0;
+        }
+        public Osoba(string name, string surname, string addr, string phone)
+        {
+            this.ime = name;
+            this.prezime = surname;
+            this.adresa = addr;
+            this.telefon = phone;
+
+            this.uk_osoba++;
+        }
+        public Osoba(string name, string surname, string addr, string phone, int height, double weight)
+        {
+            this.ime = name;
+            this.prezime = surname;
+            this.adresa = addr;
+            this.telefon = phone;
+            this.visina = height;
+            this.tezina = weight;
+
+            this.uk_osoba++;
+        }
+
+        public void ispisi_podatke()
+        {
+            Console.WriteLine("Ime: " + this.ime + "\nPrezime: " + this.prezime + "\nAdresa: " 
+            + this.adresa + "\nTelefon" + this.telefon + "\nVisina: " + this.visina +
+                "Tezina: " + this.tezina);
+        }
+    }
+
+    public class Radnik : Osoba // Klasa Radnik, nasledjuje klasu Osoba
+    {
+        double satnica;
+        int br_radnih_sati;
+        string sektor;     // Drzavno/privatno
+        string zanimanje;
+
+        public Radnik(string name, string surname, string addr, string phone, int height, 
+           double weight,double hour_rate, int hours_worked, string emp_type, string job_title)
+        {
+            this.ime = name;
+            this.prezime = surname;
+            this.adresa = addr;
+            this.telefon = phone;
+
+            this.satnica = hour_rate;
+            this.br_radnih_sati = hours_worked;
+            this.sektor = emp_type;
+            this.zanimanje = job_title;
+        }
+
+        public double izracunaj_platu()
+        {
+            return this.satnica * this.br_radnih_sati;
+        }
+
+    }
+}
---
+++ 3. godina/05. Objektno orjentisano programiranje/03. C#/03. Bankovni racun/BankovniRacun/BankovniRacun/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace BankovniRacun
+{
+    class MainClass
+    {
+        class BankovniRacun
+        {
+            public string ime;
+            private double iznos;
+
+            public BankovniRacun(string name, double amount)
+            {
+                ime = name;
+                iznos = amount;
+            }
+
+            public void upit_stanja() 
+            {
+                Console.WriteLine("Ime: " + ime + "\nStanje: " + iznos);
+            }
+
+            public void uplata(double amount)
+            {
+                iznos += amount;
+            }
+
+            public void isplata(double amount)
+            {
+                if(amount > iznos) 
+                {
+                    Console.WriteLine("Na racunu osobe '" + ime + "' nema dovoljno sredstava!");
+                }
+                else
+                {
+                    iznos -= amount;
+                }
+
+            }
+
+            public void prenos(BankovniRacun target, double amount)
+            {
+                if(iznos < amount)
+                {
+                    Console.WriteLine("Na racunu osobe '" + ime + "' nema dovoljno sredstava za prenos " + amount + " sredstava!");
+
+                }
+                else
+                {
+                    target.iznos += amount;
+                    this.iznos -= amount;
+                    Console.WriteLine("Uspesno je prebaceno " + amount + " sredstava!");
+                }
+
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            // BankovniRacun racun1 = new BankovniRacun();
+            // BankovniRacun racun1 = new BankovniRacun("Mihajlo", 500);
+            // racun1.ime = "Nemanja";
+            // racun1.iznos = -1000;
+            Console.WriteLine("Ime: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Iznos: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+            BankovniRacun racun1 = new BankovniRacun(name, amount);
+            
+            racun1.upit_stanja();
+            Console.WriteLine("Nakon sto smo skinuli 500: ");
+            racun1.isplata(500);
+            racun1.upit_stanja();
+            Console.WriteLine("Nakon sto smo uplatili 2000: ");
+            racun1.uplata(2000);
+            racun1.upit_stanja();
+
+            BankovniRacun racun2 = new BankovniRacun("Ilija", 1000);
+            racun1.prenos(racun2, 400);
+            racun1.upit_stanja();
+            racun2.upit_stanja();
+            /*
+            Pas ->
+                svojstva:
+                    ime (string)
+                    rasa (string)
+                    godine (int)
+                    boja (string)
+                    starost (string) ---
+                konstruktor (ime)
+                konstruktor (ime, godiste, rasa, boja)
+                metode:
+                    laje() -> "Av avv"
+                    jede() -> "omnom nom"
+                    ispisi_podatke() -> ispisuje sve
+                    proceni_starost() -> upisuje u starost 
+                    ("stene" (0->1) "mlad" (1->5) "zreo" (6->10) "star" (11->)
+             */
+            
+
+        }
+    }
+}
